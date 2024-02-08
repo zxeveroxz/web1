654898c96cb2d0f5817ab9efb0474350f0456006
Sergio Zegarra <s.zegarra@hotmail.com>
2024-02-08 14:27:55 -0500

    todos los componentes creados

--- src/components/AsideMenuItemtoogle.jsx
+++ src/components/AsideMenuItemtoogle.jsx
@@ -1,9 +1,14 @@
-import React, { Children } from "react";
+import {  useState } from "react";
 
-function AsideMenuItemtoogle({ children,etiqueta,active }) {
-    const activo = active?'active open':'';
+function AsideMenuItemtoogle({ children,etiqueta,active }) {    
+
+    const [activado,setActivado]= useState(active);
+    const activo = activado?'active open':'';
     return (
-        <li className={`menu-item ${activo}`}>
+        <li 
+            className={`menu-item ${activo}`}
+            onClick={() => setActivado(!activado)}
+        >
             <a href="#" className="menu-link menu-toggle">
                 <i className="menu-icon tf-icons bx bx-detail"></i>
                 <div >{etiqueta}</div>
